7ea25a3e9604bc3c99be4516c154f51c338928aa
Rhuan Lucas <197745087+rhuanlucasdev@users.noreply.github.com>
2026-09-01 12:30:37 -0300

    feat: monta a pagina inicial com navbar, secoes e rodape

---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,31 @@
+import Footer from "../../components/Footer";
+import Navbar from "../../components/Navbar";
+import Categories from "./components/Categories";
+import CompanyCTA from "./components/CompanyCTA";
+import FeaturedJobs from "./components/FeaturedJobs";
+import Hero from "./components/Hero";
+
+function Home() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <section className="bg-slate-50 py-12">
+        <div className="container mx-auto max-w-6xl px-4">
+          <div className="flex flex-col gap-10 lg:flex-row lg:items-start lg:gap-8">
+            <div className="min-w-0 flex-1">
+              <FeaturedJobs />
+            </div>
+            <div className="w-full lg:max-w-sm lg:shrink-0 flex flex-col gap-10">
+              <Categories />
+              <CompanyCTA />
+            </div>
+          </div>
+        </div>
+      </section>
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
